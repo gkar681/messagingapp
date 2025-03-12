00655a39065e98876c56508e0acbe6eec9565a81
gkar681 <gayakar681@gmail.com>
2025-03-11 23:00:29 -0700

    screens and assets set up

---
+++ frontend/screens/AllMessages.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AllMessages = () => {
+  return (
+    <View>
+      <Text>AllMessages</Text>
+    </View>
+  )
+}
+
+export default AllMessages
